5b99739140ac932ab8d770e5f354640446586893
Jack Wolfgramm <jackvwolfgramm@gmail.com>
2022-07-25 20:55:32 -1000

    Moving project to it's own Repo

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Predict business success chance based on if other businesses are opening in the area',
+    author='Jack Wolfgramm',
+    license='',
+)
